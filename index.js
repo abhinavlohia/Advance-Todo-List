4c74e5e99f147ec508f79a9a3e34f641dd433c46
abhinavlohia <helloabhinavlohia22@gmail.com>
2023-07-18 00:43:44 +0530

    Revert "added delete btn"
    This reverts commit 69f20d44b328200561b2ab41a5149712b62fca66.

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function drag(event) {
 
 let processCompleted = false;
 function drop(event, column) {
-
   processCompleted = false;
   event.preventDefault();
   const taskId = event.dataTransfer.getData("text");
@@ -55,57 +54,36 @@ function drop(event, column) {
   }
 }
 
-let taskIdCounter = parseInt(localStorage.getItem("taskIdCounter")) || 1; // Counter to generate unique task IDs
+// Restore the task positions from browser storage on page load
+window.onload = function () {
+  const tasks = JSON.parse(localStorage.getItem("tasks")) || {};
+  for (const taskId in tasks) {
+    const column = tasks[taskId];
+    const task = document.getElementById(taskId);
+    document.getElementById(column).appendChild(task);
+  }
+};
+
+let taskIdCounter = 1; // Counter to generate unique task IDs
 
 function addNewTask() {
   // const taskText = prompt("Enter the task description:"); // Prompt the user for task description
   if (1) {
     const taskId = "Task " + taskIdCounter++; // Generate unique task ID
-
     const taskElement = document.createElement("div"); // Create a new task element
     taskElement.className = "list-item";
     taskElement.draggable = true;
     taskElement.ondragstart = drag;
     taskElement.id = taskId;
+    taskElement.innerText = taskElement.id;
 
-    const taskTextElement = document.createElement("span"); // Create a span for the task text
-    taskTextElement.innerText = taskId;
-
-    const deleteButton = document.createElement("button"); // Create a delete button
-    deleteButton.className = "deleteTaskbtn";
-    deleteButton.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-    deleteButton.addEventListener("click", deleteTask);
-
-    taskElement.appendChild(taskTextElement); // Add the task text to the task element
-    taskElement.appendChild(deleteButton); // Add the delete button to the task element
-
-    const todoColumn = document.getElementById("todo"); // Get the TODO column
-    todoColumn.appendChild(taskElement); // Add the new task to the TODO column
+    document.getElementById("todo").appendChild(taskElement); // Add the new task to the TODO column
 
     // Update the task's status in the browser storage
     const tasks = JSON.parse(localStorage.getItem("tasks")) || {};
     tasks[taskId] = "todo";
     localStorage.setItem("tasks", JSON.stringify(tasks));
-
-    // Store the updated taskIdCounter in the local storage
-    localStorage.setItem("taskIdCounter", taskIdCounter.toString());
   }
-}
-
-function deleteTask(event) {
-  const taskElement = event.target.closest(".list-item"); // Find the closest parent task element
-  const taskId = taskElement.id;
-
-  // Remove the task element from the DOM
-  if (taskElement.parentNode.id === 'todo') {
-    taskElement.remove();
-
-    // Remove the task from the browser storage
-    const tasks = JSON.parse(localStorage.getItem("tasks")) || {};
-    delete tasks[taskId];
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }
-
 }
 
 // Restore the task positions from browser storage on page load
@@ -113,30 +91,19 @@ window.onload = function () {
   const tasks = JSON.parse(localStorage.getItem("tasks")) || {};
   for (const taskId in tasks) {
     const column = tasks[taskId];
-    const taskElement = document.createElement("div"); // Create a new task element
-    taskElement.className = "list-item";
-    taskElement.draggable = true;
-    taskElement.ondragstart = drag;
-    taskElement.id = taskId;
-
-    const taskTextElement = document.createElement("span"); // Create a span for the task text
-    taskTextElement.innerText = taskId;
-
-    const deleteButton = document.createElement("button"); // Create a delete button
-    deleteButton.className = "deleteTaskbtn";
-    deleteButton.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-    deleteButton.addEventListener("click", deleteTask);
-
-    taskElement.appendChild(taskTextElement); // Add the task text to the task element
-    taskElement.appendChild(deleteButton); // Add the delete button to the task element
+    const task = document.createElement("div");
+    task.className = "list-item";
+    task.draggable = true;
+    task.ondragstart = drag;
+    task.id = taskId;
+    task.innerText = taskId;
 
     const columnElement = document.getElementById(column);
     if (columnElement) {
-      columnElement.appendChild(taskElement);
+      columnElement.appendChild(task);
     }
   }
 };
-
 
 function deleteAllTask() {
   localStorage.clear();
